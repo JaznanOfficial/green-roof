bec3c84afb11c5d3d4c01865507fd37e22305875
FahimFaisalKhan <khanfahimfaisal@gmail.com>
2023-01-19 02:18:46 +0600

    file cleaned

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
 import TopNavigation from "./components/Navigation/TopNavigation";
 import Routers from "./routers/Routers";
 import Footer from "./components/Footer/Footer";
--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -11,25 +11,25 @@ import SignUpPage from "../pages/SignUpPage";
 import InvoicePage from "../pages/InvoicePage";
 import RulesPage from "../pages/RulesPage";
 import UserProfilePage from "../pages/UserProfilePage";
-import NotFoundPage from '../pages/NotFoundPage';
+import NotFoundPage from "../pages/NotFoundPage";
 
 const Routers = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Homepage />}></Route>
-            <Route path="/about" element={<AboutPage />}></Route>
-            <Route path="/products/" element={<AllProductsPage />}></Route>
-            <Route path="/products/:id" element={<SingleProductPage />}></Route>
-            <Route path="/contact" element={<ContactPage />}></Route>
-            <Route path="/checkout" element={<CheckoutPage />}></Route>
-            <Route path="/sign-in" element={<SignInPage />}></Route>
-            <Route path="/sign-up" element={<SignUpPage />}></Route>
-            <Route path="/invoice/:id" element={<InvoicePage />}></Route>
-            <Route path="/rules" element={<RulesPage />}></Route>
-            <Route path="/user" element={<UserProfilePage />}></Route>
-            <Route path="*" element={<NotFoundPage />}></Route>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />}></Route>
+      <Route path="/about" element={<AboutPage />}></Route>
+      <Route path="/products" element={<AllProductsPage />}></Route>
+      <Route path="/products/:id" element={<SingleProductPage />}></Route>
+      <Route path="/contact" element={<ContactPage />}></Route>
+      <Route path="/checkout" element={<CheckoutPage />}></Route>
+      <Route path="/sign-in" element={<SignInPage />}></Route>
+      <Route path="/sign-up" element={<SignUpPage />}></Route>
+      <Route path="/invoice/:id" element={<InvoicePage />}></Route>
+      <Route path="/rules" element={<RulesPage />}></Route>
+      <Route path="/user" element={<UserProfilePage />}></Route>
+      <Route path="*" element={<NotFoundPage />}></Route>
+    </Routes>
+  );
 };
 
 export default Routers;
