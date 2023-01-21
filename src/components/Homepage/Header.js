b4472fadcb340d4f70d06401fa973bec4eabeecf
JaznanOfficial <jaznanofficial@gmail.com>
2023-01-21 23:22:27 +0600

    added carousel

--- src/components/Homepage/Header.js
+++ src/components/Homepage/Header.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import { Carousel } from "flowbite-react";
+import React from "react";
 
 const Header = () => {
     return (
-        <div>
-            header
-            header
-            header
-            header
-        </div>
+        <>
+            <div className="h-96 sm:h-64 xl:h-80 2xl:h-96 p-2 rounded-xl ">
+                <Carousel className="">
+                    <img
+                        className=""
+                        src="http://wp.alithemes.com/html/nest/demo/assets/imgs/slider/slider-1.png"
+                        alt="..."
+                    />
+                    <img
+                        src="http://wp.alithemes.com/html/nest/demo/assets/imgs/slider/slider-2.png"
+                        alt="..."
+                    />
+                </Carousel>
+            </div> 
+
+            
+        </>
     );
 };
 
