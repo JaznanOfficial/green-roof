2bda9b287bae2ea9cf897e88208c391763f214a0
JaznanOfficial <jaznanofficial@gmail.com>
2023-01-22 12:36:31 +0600

    carousel add

--- src/components/Homepage/Header.js
+++ src/components/Homepage/Header.js
@@ -4,14 +4,16 @@ import React from "react";
 const Header = () => {
     return (
         <>
-            <div className="h-96 sm:h-64 xl:h-80 2xl:h-96 p-2 rounded-xl ">
-                <Carousel className="">
+            <div className="h-96 sm:h-64 xl:h-80 2xl:h-96 p-2 rounded-3xl ">
+                
+                <Carousel style={{height:"450px"}} className="rounded-3xl">
                     <img
-                        className=""
+                        className="rounded-3xl"
                         src="http://wp.alithemes.com/html/nest/demo/assets/imgs/slider/slider-1.png"
                         alt="..."
                     />
                     <img
+                    className="rounded-3xl"
                         src="http://wp.alithemes.com/html/nest/demo/assets/imgs/slider/slider-2.png"
                         alt="..."
                     />
